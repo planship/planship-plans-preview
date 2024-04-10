cb73e8f78f016a7beaade708247a35f8ebc67233
Pawel Wojnarowicz <pawel.wojnarowicz@gmail.com>
2024-04-10 10:16:59 +0200

    Add icons to the grid/column view toggle.

--- pages/[slug]/index.vue
+++ pages/[slug]/index.vue
@@ -7,6 +7,8 @@ import {
   Switch,
 } from '@headlessui/vue'
 
+import { TableCellsIcon, ViewColumnsIcon } from '@heroicons/vue/24/outline'
+
 import { usePlanshipStore } from '@/stores/planship'
 
 const planshipStore = usePlanshipStore()
@@ -23,18 +25,26 @@ const isGridView = ref(true)
 </script>
 
 <template>
-  <div class=" justify-end hidden md:flex">
+  <div class=" justify-end hidden md:flex px-4">
+    <div class="flex items-center">
+      <span class="sr-only">Show as columns</span>
+      <ViewColumnsIcon class="h-6 w-6" aria-hidden="true" />
+    </div>
     <Switch
       v-model="isGridView"
       :class="isGridView ? 'bg-blue-600' : 'bg-gray-200'"
       class="m-2 relative inline-flex h-6 w-11 items-center rounded-full"
     >
-      <span class="sr-only">Show as grid</span>
       <span
         :class="isGridView ? 'translate-x-6' : 'translate-x-1'"
         class="inline-block h-4 w-4 transform rounded-full bg-white transition"
       />
     </Switch>
+    <div class="flex items-center">
+      <span class="sr-only">Show as grid</span>
+      <TableCellsIcon class="h-6 w-6" aria-hidden="true" />
+    </div>
+
   </div>
   <PlansGridView @change-plan="changePlan" class="hidden md:block" v-if="isGridView" />
   <PlansListView @change-plan="changePlan"  class="hidden md:block" v-else/>
